cbf769b893a6133a44b2af3a0393e0ea1413a440
Namrata Shinde <namratapshinde48@gmail.com>
2025-12-20 18:38:07 +0530

    new programs

---
+++ PrintLargestOfThree.java
@@ -0,0 +1,14 @@
+public class PrintLargestOfThree{
+    public static void main(String args[]){
+        int A=18, B=30,C=6;
+        if ((A>=B)&&(A>=C)){
+            System.out.println("A is largest!");
+        }
+        else if (B>=C){
+            System.out.println("B is largest!");
+        }
+        else {
+            System.out.println("C is largest!");
+        }
+    }
+}
