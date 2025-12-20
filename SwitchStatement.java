8743e41520d4d9fb6cecebc9486ea19b2755aebf
Namrata Shinde <namratapshinde48@gmail.com>
2025-12-20 23:08:26 +0530

    Create SwitchStatement.java

---
+++ SwitchStatement.java
@@ -0,0 +1,15 @@
+public class SwitchStatement{
+    public static void main(String args[]){
+        int number=2;
+        switch(number){
+            case 1: System.out.println("Samosa");
+            break;
+            case 2: System.out.println("Burger");
+            break;
+            case 3: System.out.println("MangoShake");
+            break;
+            default : System.out.println("we wake up");
+            break;
+        }
+    }
+}
