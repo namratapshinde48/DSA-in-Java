9cfab3e423c910c2e59f3764b7a686641c896373
Namrata Shinde <namratapshinde48@gmail.com>
2025-12-21 15:00:10 +0530

    Create PrintSquarePatternWithWhileLoop.java

---
+++ PrintSquarePatternWithWhileLoop.java
@@ -0,0 +1,9 @@
+public class PrintSquarePatternWithWhileLoop{
+    public static void main(String args[]){
+        int line=1;
+        while(line<=4){
+            System.out.println("****");
+            line++;
+        }
+    }
+}
