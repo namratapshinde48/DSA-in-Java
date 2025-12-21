7021429d2cc7106e0933dbdc2ce8aaa9ee88b654
Namrata Shinde <namratapshinde48@gmail.com>
2025-12-21 13:33:06 +0530

    Create PrintOneToHundred.java

---
+++ PrintOneToHundred.java
@@ -0,0 +1,9 @@
+public class PrintOneToHundred{
+    public static void main(String args[]){
+        int counter=1;
+        while (counter<=100){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
